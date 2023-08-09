7caf814973661024afe25bb71a9e8b0815e9727b
Serhii Voichenko <38272208+Saggitta@users.noreply.github.com>
2023-08-09 16:25:39 +0300

    Implement Oauth2

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 import os
 
-from flask import Flask
+from flask import Flask, session
 
+from config import config
+from auth.views import auth_bp
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    app.secret_key = config["WEBAPP"]["SECRET_KEY"]
     app.config.from_mapping(
         SECRET_KEY="dev", DATABASE=os.path.join(app.instance_path, "flaskr.sqlite")
     )
@@ -22,18 +25,23 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-        # a simple page that says hello
 
     @app.route("/hello")
     def hello():
-        return "Hello, World!"
+        return session.get('user')
 
     from . import db
 
     db.init_app(app)
 
-    from . import auth
+    # from . import auth
 
-    app.register_blueprint(auth.bp)
-    
+    # app.register_blueprint(auth.bp)
+
+    from . import notes
+
+    app.register_blueprint(notes.bp)
+    app.register_blueprint(auth_bp, url_prefix='/')
+    app.add_url_rule("/", endpoint="index")
+
     return app
--- app/notes.py
+++ app/notes.py
@@ -1,37 +1,43 @@
-from flask import Blueprint, flash, g, redirect, render_template, request, url_for
+from flask import Blueprint, session
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
 from werkzeug.exceptions import abort
 
-from app.auth import login_required
 from app.db import get_db
+from auth.decorators import requires_auth
 
 bp = Blueprint("notes", __name__)
 
 
-@bp.route("/create", methods=("GET", "POST"))
-@login_required
-def create():
-    if request.method == "POST":
-        title = request.form["title"]
-        body = request.form["body"]
-        error = None
-    if not title:
-        error = "Title is required."
-    if error is not None:
-        flash(error)
-    else:
-        db = get_db()
-        db.execute(
-            "INSERT INTO notes (title, body, author_id)" " VALUES (?, ?, ?)",
-            (title, body, g.user["id"]),
-        )
-        db.commit()
-        return redirect(url_for("blog.index"))
-
-    return render_template("blog/create.html")
+@bp.route("/")
+def index():
+    """Show all the notes, most recent first."""
+    db = get_db()
+    notes = db.execute(
+        "SELECT p.id, title, body, created, author_id, username"
+        " FROM notes p JOIN user u ON p.author_id = u.id"
+        " ORDER BY created DESC"
+    ).fetchall()
+    return render_template("notes/index.html", notes=notes)
 
 
-def get_note(id, check_author=True):
-    note = (
+def get_post(id, check_author=True):
+    """Get a notes and its author by id.
+
+    Checks that the id exists and optionally that the current user is
+    the author.
+
+    :param id: id of notes to get
+    :param check_author: require the current user to be the author
+    :return: the notes with author information
+    :raise 404: if a notes with the given id doesn't exist
+    :raise 403: if the current user isn't the author
+    """
+    notes = (
         get_db()
         .execute(
             "SELECT p.id, title, body, created, author_id, username"
@@ -42,18 +48,49 @@ def get_note(id, check_author=True):
         .fetchone()
     )
 
-    if note is None:
-        abort(404, f"Note id {id} doesn't exist.")
+    if notes is None:
+        abort(404, f"Post id {id} doesn't exist.")
 
-    if check_author and note["author_id"] != g.user["id"]:
+    if check_author and notes["author_id"] != g.user["id"]:
         abort(403)
 
-    return note
+    return notes
 
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
-@login_required
+
+@bp.route("/create", methods=("GET", "POST"))
+@requires_auth
+def create():
+    """Create a new notes for the current user."""
+    if request.method == "POST":
+        title = request.form["title"]
+        body = request.form["body"]
+        error = None
+
+        author_id = "1234"
+
+        if not title:
+            error = "Title is required."
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                "INSERT INTO notes (title, body, author_id) VALUES (?, ?, ?)",
+                (title, body, author_id),
+            )
+            db.commit()
+            return redirect(url_for("notes.index"))
+
+    return render_template("notes/create.html")
+
+
+@bp.route("/<int:id>/update", methods=("GET", "POST"))
+@requires_auth
 def update(id):
-    post = get_note(id)
+    """Update a notes if the current user is the author."""
+    notes = get_post(id)
+
     if request.method == "POST":
         title = request.form["title"]
         body = request.form["body"]
@@ -67,18 +104,24 @@ def update(id):
         else:
             db = get_db()
             db.execute(
-                "UPDATE notes SET title = ?, body = ?" " WHERE id = ?", (title, body, id)
+                "UPDATE notes SET title = ?, body = ? WHERE id = ?", (title, body, id)
             )
-        db.commit()
-        return redirect(url_for("notes.index.html"))
-    return render_template("notes/update.html", post=post)
+            db.commit()
+            return redirect(url_for("notes.index"))
+
+    return render_template("notes/update.html", notes=notes)
 
 
-@bp.route('/<int:id>/delete', methods=('POST',))
-@login_required
+@bp.route("/<int:id>/delete", methods=("POST",))
+@requires_auth
 def delete(id):
-    get_note(id)
+    """Delete a notes.
+
+    Ensures that the notes exists and that the logged in user is the
+    author of the notes.
+    """
+    get_post(id)
     db = get_db()
-    db.execute('DELETE FROM notes WHERE is = ?', (id))
+    db.execute("DELETE FROM notes WHERE id = ?", (id,))
     db.commit()
-    return redirect(url_for('notes.index'))
+    return redirect(url_for("notes.index"))
